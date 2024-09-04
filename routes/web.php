febbb861c3035656884dfc1a2eb3f7c81c19d4ca
latif bima <latiefbima18@gmail.com>
2024-09-04 10:06:09 +0700

    jobsheet 2 praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () {
+    return 'Hello World';
+   });
+
+Route::get('/world', function () {
+    return 'World';
+   });
+
+   Route::get('/welcome', function () {
+    return 'selamat datang';
+   });
+   
+   Route::get('/about', function () {
+    return 'BIMA NIM 2241760047';
+   });
+
+   Route::get('/user/{name}', function ($name) {
+    return 'Nama Saya '.$name;
+    });
+    
+    Route::get('/posts/{post}/comments/{comment}', function
+($postId, $commentId) {
+ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID $id";
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });
+    
+
